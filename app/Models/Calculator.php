ab02352b5042951cfacbe8d85c2ab5b779320885
gilzilberfeld <gil.zilberfeld@gmail.com>
2020-11-17 11:22:43 +0200

    added factorial

--- app/Models/Calculator.php
+++ app/Models/Calculator.php
@@ -3,39 +3,48 @@ namespace App\Models;
 
 class Calculator
 {
-    var $a;
-    var $b;
+    var $param1;
+    var $param2;
 
-    function checkopration($oprator)
+    function calculate($param1, $param2, $op)
     {
-        switch($oprator)
+        $this->param1 = $param1;
+        $this->param2 = $param2;
+        return $this->doCalculation($op);
+    }
+
+    function doCalculation($operator)
+    {
+        switch($operator)
         {
             case '+':
-                return $this->a + $this->b;
+                return $this->param1 + $this->param2;
                 break;
 
             case '-':
-                return $this->a - $this->b;
+                return $this->param1 - $this->param2;
                 break;
 
             case '*':
-                return $this->a * $this->b;
+                return $this->param1 * $this->param2;
                 break;
 
             case '/':
-                return $this->a / $this->b;
+                return $this->param1 / $this->param2;
                 break;
 
+            case 'f':
+                return $this->factorial($this->param1);
             default:
                 return "Sorry No command found";
         }
     }
-    function calculate($a, $b, $c)
+
+    function factorial($n)
     {
-        $this->a = $a;
-        $this->b = $b;
-        return $this->checkopration($c);
+        return $n <= 1 ? 1 : $n * $this->factorial($n - 1);
     }
+
 }
 
 
